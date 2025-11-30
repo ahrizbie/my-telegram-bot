69443f2c246dad9aef8e17250a84d0ed8cbcf8c3
ahrizbie <ahgamer1971@gmail.com>
2025-12-01 05:30:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Your golden coin (uploaded by you)
 const GOLDEN_COIN_FILE_ID = 'AgACAgUAAxkBAAE-mmlpLMeylG0_2uo_Bz0TwYSPgan02wAC5g5rG3T9WFXKCfM5sJ0I2QEAAwIAA3MAAzYE';
 
 bot.start((ctx) => {
-  const name = (ctx.from.first_name || ctx.from.username || 'Player').replace(/([_*[\]()~`>#+=|{}.!\\-])/g, '\\$1');
+  const name = (ctx.from.first_name || ctx.from.username || 'Player')
+    .replace(/([_*[\]()~`>#+=|{}.!\\-])/g, '\\$1');
 
   ctx.replyWithPhoto(
     GOLDEN_COIN_FILE_ID,
@@ -18,12 +18,14 @@ bot.start((ctx) => {
         `\\$YOURCOIN is what you want it to be\\. That’s all you need to know\\.`,
       parse_mode: 'MarkdownV2',
       reply_markup: {
-        inline_keyboard: [[
-          {
-            text: 'Launch \\$YOURCOIN',
-            web_app: { url: 'https://t.me/Notgames_tools_bot/app' }
-          }
-        ]]
+        inline_keyboard: [
+          [
+            {
+              text: 'Launch \\$YOURCOIN',
+              url: 'https://t.me/Notgames_tools_bot/app'   // ← THIS WORKS 100%
+            }
+          ]
+        ]
       }
     }
   );
@@ -32,6 +34,5 @@ bot.start((ctx) => {
 bot.launch();
 console.log('Bot is running 24/7');
 
-// Required for Render (prevents "No open ports" restarts)
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
