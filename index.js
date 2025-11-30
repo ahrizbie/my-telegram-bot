d7f5ecb761dac8b794f21105190f11ff286b0f74
ahrizbie <ahgamer1971@gmail.com>
2025-12-01 05:26:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Your personal golden coin (you already uploaded it)
+// Your golden coin (uploaded by you)
 const GOLDEN_COIN_FILE_ID = 'AgACAgUAAxkBAAE-mmlpLMeylG0_2uo_Bz0TwYSPgan02wAC5g5rG3T9WFXKCfM5sJ0I2QEAAwIAA3MAAzYE';
 
 bot.start((ctx) => {
-  const name = ctx.from.first_name || '@' + ctx.from.username || 'Player';
+  const name = (ctx.from.first_name || ctx.from.username || 'Player').replace(/([_*[\]()~`>#+=|{}.!\\-])/g, '\\$1');
 
   ctx.replyWithPhoto(
     GOLDEN_COIN_FILE_ID,
     {
-      caption: `Hi, *${name.replace(/([_*[\]()~`>#+=|{}.!-])/g, '\\$1')}*! This is YOURCOIN 👋\n\n` +
-               `Tap on the coin and watch your balance grow.\n\n` +
-               `How much is $YOURCOIN worth? No one knows, probably nothing 😂\n\n` +
-               `Got any friends? Get them in the game. That way you'll get even more coins together.\n\n` +
-               `$YOURCOIN is what you want it to be. That’s all you need to know.`,
+      caption:
+        `Hi, *${name}*\\! This is YOURCOIN\n\n` +
+        `Tap on the coin and watch your balance grow\\.\n\n` +
+        `How much is \\$YOURCOIN worth\\? No one knows, probably nothing\n\n` +
+        `Got any friends\\? Get them in the game\\. That way you'll get even more coins together\\.\n\n` +
+        `\\$YOURCOIN is what you want it to be\\. That’s all you need to know\\.`,
       parse_mode: 'MarkdownV2',
       reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: 'Launch $YOURCOIN',
-              web_app: { url: 'https://t.me/Notgames_tools_bot/app' }   // ← THIS WORKS NOW
-            }
-          ]
-        ]
+        inline_keyboard: [[
+          {
+            text: 'Launch \\$YOURCOIN',
+            web_app: { url: 'https://t.me/Notgames_tools_bot/app' }
+          }
+        ]]
       }
     }
   );
 });
 
-// Graceful stop (very important on Render)
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
 bot.launch();
 console.log('Bot is running 24/7');
+
+// Required for Render (prevents "No open ports" restarts)
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
